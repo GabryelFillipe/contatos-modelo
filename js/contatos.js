cef3c784394771ae90f4df7d1a6502831421b35c
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-11-10 11:11:36 -0300

    Desenvolvendo logíca para criação e visualização dos contatos na pagína

---
+++ js/contatos.js
@@ -0,0 +1,73 @@
+export async function lerContatos() {
+
+    const url = "https://bakcend-fecaf-render.onrender.com/contatos"
+    const response = await fetch(url)
+    const contatos = await response.json()
+
+    return contatos
+}
+
+export async function criarContato(contato) {
+
+    const url = "https://bakcend-fecaf-render.onrender.com/contatos"
+
+    const options = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(contato)
+    }
+
+    const response = await fetch(url, options)
+
+
+    return response.ok
+
+}
+
+ async function atualizarContato(id, contato) {
+    
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+
+    const options = {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(contato)
+    }
+
+    const response = await fetch(url, options)
+
+
+    return response.ok
+
+}
+
+export async function deletarContato(id) {
+    
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+
+    const options = {
+        method: "DELETE"
+    }
+
+    const response = await fetch(url, options)
+
+
+    return response.ok
+
+}
+
+
+
+const novoContato = {
+    "nome": "Gabryel Fillipe",
+    "celular": "11 9 7171-6464",
+    "foto": "semFoto.png",
+    "email": "GabryelF@gmail.com",
+    "endereco": "Rua Europa, 234",
+    "cidade": "Carapicuiba"
+}
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,124 @@
 'use strict'
+
+import { lerContatos } from "./contatos.js"
+import { criarContato } from "./contatos.js"
+import { deletarContato } from "./contatos.js"
+
+function criarCardContato(contato) {
+
+    const cardContato = document.createElement('div')
+    cardContato.classList.add('card-contato')
+    cardContato.id = 'card-contato'
+
+    let img = contato.foto
+    if (img == null || img == undefined || img.includes('semFoto')) {
+        img = '../img/avatar1.avif'
+    }
+    img.onerror = function(){
+        this.src = '../img/avatar1.avif'
+    }
+
+    const fotoPerfil = document.createElement('img')
+    fotoPerfil.src = img
+
+    const nomeContato = document.createElement('h2')
+    nomeContato.textContent = contato.nome
+
+    const numeroContato = document.createElement('p')
+    numeroContato.textContent = contato.celular
+
+    cardContato.appendChild(fotoPerfil)
+    cardContato.appendChild(nomeContato)
+    cardContato.appendChild(numeroContato)
+
+    return cardContato
+
+}
+
+async function popularContatos() {
+
+    const containerContatos = document.getElementById('container')
+
+    const contatos = await lerContatos()
+
+    contatos.forEach(contato => {
+
+        const cardContato = criarCardContato(contato)
+
+        containerContatos.appendChild(cardContato)
+    })
+}
+
+async function criarNovoContato(contato) {
+
+
+    const novoContato = criarContato(contato)
+
+
+    const main = document.querySelector('main')
+    main.classList.replace('form-show', 'card-show')
+    await popularContatos()
+    return novoContato
+
+}
+
+async function atualizarCard() {
+
+    const cardContato = document.querySelectorAll('.card-contato')
+
+    cardContato.forEach(card =>{
+       const cardEscolhido =  card.target('.card-contato')
+       console.log(cardEscolhido)
+    })
+
+}
+
+const cadastrarContato = document.getElementById('novo-contato')
+
+cadastrarContato.addEventListener('click', () => {
+
+    const main = document.querySelector('main')
+    main.classList.replace('card-show', 'form-show')
+
+})
+
+const salvar = document.getElementById('salvar')
+
+salvar.addEventListener('click', async () => {
+
+    const nome = document.getElementById('nome')
+    const email = document.getElementById('email')
+    const celular = document.getElementById('celular')
+    const endereco = document.getElementById('endereco')
+    const cidade = document.getElementById('cidade')
+    const foto = document.getElementById('foto')
+
+    if (nome.value == '' || email.value == '')
+        alert('campos obrigatorios não preenchidos')
+
+    const novoContato = {
+        nome: nome.value,
+        email: email.value,
+        celular: celular.value,
+        endereco: endereco.value,
+        cidade: cidade.value,
+        foto: '../img/avatar1.avif'
+    }
+    await criarNovoContato(novoContato)
+
+
+})
+
+const cancelar = document.getElementById('cancelar')
+
+
+cancelar.addEventListener('click', () => {
+    const main = document.querySelector('main')
+    main.classList.replace('form-show', 'card-show')
+})
+
+atualizarCard()
+popularContatos()
+
+
+
