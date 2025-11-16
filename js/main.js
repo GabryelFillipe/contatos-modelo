89a7adf953dde43dfd95a0441d4c004e2eedc6a2
Gabryel Fillipe <byelfcsilva@gmail.com>
2025-11-16 12:34:40 -0300

    Desenvolvendo logica para editar um contato e adaptando o codigo aos padrões clean code e SRP

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@
 
 import { lerContatos } from "./contatos.js"
 import { criarContato } from "./contatos.js"
+import { atualizarContato } from "./contatos.js"
 import { deletarContato } from "./contatos.js"
 
 function criarCardContato(contato) {
 
     const cardContato = document.createElement('div')
     cardContato.classList.add('card-contato')
-    cardContato.id = 'card-contato'
+    cardContato.id = contato.nome
 
     const imagemReserva = '../img/erro.webp'
     let img = contato.foto
     if (img == null || img == undefined || img.includes('semFoto')) {
         img = '../img/avatar1.avif'
     }
-
 
     const fotoPerfil = document.createElement('img')
     fotoPerfil.onerror = function () {
@@ -38,89 +38,203 @@ function criarCardContato(contato) {
     cardContato.appendChild(numeroContato)
 
     return cardContato
-
 }
 
 async function popularContatos() {
 
     const containerContatos = document.getElementById('container')
+    containerContatos.replaceChildren()
 
     const contatos = await lerContatos()
 
     contatos.forEach(contato => {
-
         const cardContato = criarCardContato(contato)
-
         containerContatos.appendChild(cardContato)
     })
 }
 
-async function criarNovoContato(contato) {
+function preencherFormulario(contato) {
 
+    document.getElementById('formulario').dataset.id = contato.id
 
-    const novoContato = criarContato(contato)
+    const fotoPreview = document.getElementById('preview-image')
+    if (contato.foto) {
+        fotoPreview.src = contato.foto
+    } else {
+        fotoPreview.src = './img/preview-icon.png'
+    }
 
+    const nomeContato = document.getElementById('nome')
+    nomeContato.value = contato.nome || ''
+    nomeContato.readOnly = true
 
-    const main = document.querySelector('main')
-    main.classList.replace('form-show', 'card-show')
-    await popularContatos()
-    return novoContato
+    const emailContato = document.getElementById('email')
+    emailContato.value = contato.email || ''
+    emailContato.readOnly = true
 
-}
+    const celularContato = document.getElementById('celular')
+    celularContato.value = contato.celular || ''
+    celularContato.readOnly = true
 
-async function atualizarCard() {
+    const enderecoContato = document.getElementById('endereco')
+    enderecoContato.value = contato.endereco || ''
+    enderecoContato.readOnly = true
 
-    const cardContato = document.getElementById('card-contato')
-    console.log(cardContato)
-
-}
-
-const cadastrarContato = document.getElementById('novo-contato')
-
-cadastrarContato.addEventListener('click', () => {
+    const cidadeContato = document.getElementById('cidade')
+    cidadeContato.value = contato.cidade || ''
+    cidadeContato.readOnly = true
 
     const main = document.querySelector('main')
     main.classList.replace('card-show', 'form-show')
+}
 
-})
+async function carregarDadosContato(cardEscolhido) {
 
-const salvar = document.getElementById('salvar')
+    const nomeContato = cardEscolhido.id
+    const listaContatos = await lerContatos()
 
-salvar.addEventListener('click', async () => {
+    const infoContato = listaContatos.find(contato => {
+        return contato.nome === nomeContato
+    })
 
-    const nome = document.getElementById('nome')
-    const email = document.getElementById('email')
-    const celular = document.getElementById('celular')
-    const endereco = document.getElementById('endereco')
-    const cidade = document.getElementById('cidade')
-    const foto = document.getElementById('foto')
+    if (infoContato) {
+        let contato = {
+            id: infoContato.id,
+            nome: infoContato.nome,
+            email: infoContato.email,
+            celular: infoContato.celular,
+            endereco: infoContato.endereco,
+            cidade: infoContato.cidade
+        }
+        preencherFormulario(contato)
+    } else {
+        alert('ERRO: CONTATO NÃO ENCONTRADO NA LISTA')
+    }
+}
 
-    if (nome.value == '' || email.value == '')
-        alert('campos obrigatorios não preenchidos')
+function prepararFormularioParaNovoContato() {
+    
+    const formulario = document.getElementById('formulario')
+    
+    
+    formulario.dataset.id = ''
+    
+    
+    formulario.reset() 
 
-    const novoContato = {
-        nome: nome.value,
-        email: email.value,
-        celular: celular.value,
-        endereco: endereco.value,
-        cidade: cidade.value,
+    
+    document.getElementById('preview-image').src = './img/preview-icon.png'
+    document.getElementById('foto').value = null
+
+    
+    document.getElementById('nome').readOnly = false
+    document.getElementById('email').readOnly = false
+    document.getElementById('celular').readOnly = false
+    document.getElementById('endereco').readOnly = false
+    document.getElementById('cidade').readOnly = false
+
+    
+    const main = document.querySelector('main')
+    main.classList.replace('card-show', 'form-show')
+}
+
+function lerDadosDoFormulario() {
+    
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+
+    
+    if (nome == '' || email == '') {
+        alert('Campos obrigatórios (Nome e E-mail) não preenchidos')
+        return null
+    }
+
+    
+    const celular = document.getElementById('celular').value
+    const endereco = document.getElementById('endereco').value
+    const cidade = document.getElementById('cidade').value
+
+    
+    const contato = {
+        nome: nome,
+        email: email,
+        celular: celular,
+        endereco: endereco,
+        cidade: cidade,
         foto: '../img/avatar1.avif'
     }
-    await criarNovoContato(novoContato)
+    
+    return contato
+}
 
+function limparEFecharFormulario() {
+    const formulario = document.getElementById('formulario')
+    
+    
+    formulario.reset()
+    
+    
+    formulario.dataset.id = ''
+    
+    
+    document.getElementById('preview-image').src = './img/preview-icon.png'
+    document.getElementById('foto').value = null
 
-})
-
-const cancelar = document.getElementById('cancelar')
-
-
-cancelar.addEventListener('click', () => {
+    
     const main = document.querySelector('main')
     main.classList.replace('form-show', 'card-show')
+}
+
+const cadastrarContato = document.getElementById('novo-contato')
+cadastrarContato.addEventListener('click', prepararFormularioParaNovoContato)
+
+
+const container = document.getElementById('container')
+container.addEventListener('click', () => {
+    let cardEscolhido = event.target.closest('.card-contato')
+    carregarDadosContato(cardEscolhido)
+})
+
+
+const salvar = document.getElementById('salvar')
+salvar.addEventListener('click', async () => {
+
+    const formulario = document.getElementById('formulario')
+    const idEdicao = formulario.dataset.id
+    
+    
+    const contato = lerDadosDoFormulario()
+    
+    
+    if (contato === null) return
+
+    
+    if (idEdicao) {
+        await atualizarContato(idEdicao, contato)
+    } else {
+        await criarContato(contato)
+    }
+    
+    
+    limparEFecharFormulario()
+
+    
+    await popularContatos()
+})
+
+
+const cancelar = document.getElementById('cancelar')
+cancelar.addEventListener('click', limparEFecharFormulario)
+
+
+const editar = document.getElementById('editar')
+editar.addEventListener('click', () => {
+    
+    document.getElementById('nome').readOnly = false
+    document.getElementById('email').readOnly = false
+    document.getElementById('celular').readOnly = false
+    document.getElementById('endereco').readOnly = false
+    document.getElementById('cidade').readOnly = false
 })
 
 popularContatos()
-atualizarCard()
-
-
-
